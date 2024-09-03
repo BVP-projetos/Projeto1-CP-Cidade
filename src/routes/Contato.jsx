24a63dfc70f742fcf2b6158f98fed046baad7ee6
victor <victorcapp15@gmail.com>
2024-09-03 15:25:11 -0300

    fazendo a base da pagina contato

--- src/routes/Contato.jsx
+++ src/routes/Contato.jsx
@@ -2,6 +2,12 @@ const Contato=()=>{
     return(
         <>
         <h1>Página Contato</h1>
+        <h2>Telefone: (11) 3456-7890  </h2>
+        <h2>Email: contato@sportworld.com.br </h2>
+        <h2>Endereço: Rua das Palmeiras, 1234 - Bairro Jardim Paulista, São Paulo - SP, CEP 01423-001 </h2>
+        <h2>Instagram: LojaSportWorld </h2>
+        <h2>Facebook: SportWorld.io </h2>
+        <h2>Parceiros: Sportingbet </h2>
         </>
     )
 }
