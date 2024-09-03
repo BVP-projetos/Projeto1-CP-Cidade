d8ba4eee2b1a4dd56cb80f238bfe9abea5977280
victor <victorcapp15@gmail.com>
2024-09-03 17:28:31 -0300

    Adicionando imagens ao Home e  Produtos + arrumando Sobre.jsx

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,7 +1,11 @@
+import Image3 from '../assets/SportWorld.jpg'
+
 const Home=()=>{
     return(
         <>
         <h1>Página Home - Sport World</h1>
+
+        <img src={Image3} alt="Home"/>
         </>
     )
 }
--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,7 +1,10 @@
+import Image4 from '../assets/Acessorios.jpg'
+
 const Produtos=()=>{
     return(
         <>
         <h1>Página Produtos</h1>
+        <img src={Image4} alt="Acessórios"/>
         </>
     )
 }
